f764e4a5798d38d0e80e87f34b14fa626b31d30f
githup98 <cda25712@gmail.com>
2022-08-04 13:59:26 +0700

    [A] add code cast data to array

---
+++ readFile.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void red()
+{
+	printf("\033[0;31m");
+}
+
+void yellow()
+{
+	printf(" Yellow \033[0;33m");
+}
+
+void reset()
+{
+	printf("\033[0m");
+}
+
+int main()
+{
+    	
+	//system("COLOR 06");
+	/////////////// change color //////////////////
+	
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hStdOut, (FOREGROUND_BLUE | BACKGROUND_GREEN));
+
+    printf("I'm BLUE ");
+
+    SetConsoleTextAttribute(hStdOut, (7 | 0)); // chu trang nen den (default)
+
+    printf("and I'm RED ");
+
+    SetConsoleTextAttribute(hStdOut, (BACKGROUND_GREEN));
+
+    printf("and we both have green background!\r\n");
+	///////////////////
+	
+	
+	char line[20];
+    char array20[20][20];
+    int numberOfLine = 0;
+    FILE *fd = fopen("dataFile.txt", "r");
+    unsigned char breakWhile = 1;
+    unsigned char ii = 0;
+    while(breakWhile)
+    {
+        
+        for(ii = 0; ii < 20; ii++)
+		{
+			if(fgets(line, 20, fd) != NULL)
+			{
+				//array20 = (char)line;
+				line[strlen(line) - 1] = '\0';
+				strncpy(array20[ii], line, strlen(line));
+				printf("%s ", line);
+				strcpy(array20[ii], line);
+				numberOfLine ++;
+			}
+			else
+			{
+				breakWhile = 0;
+				break;
+			}
+		}
+		printf("\n");
+    }
+    close(fd);
+    printf("total line: %u\n", numberOfLine);
+    return 0;
+}
