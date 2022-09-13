3524dbedf5bb7b0f7b15a4f62c1b53d46e93e908
Truc Do <cda25712@gmail.com>
2022-09-13 19:06:46 +0700

    upd sim

--- simpleDriver.c
+++ simpleDriver.c
@@ -4,8 +4,19 @@
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/delay.h>
+#include <asm-generic/io.h>
+////#include <sys/io.h>
 #define  DEVICE_NAME "ebbchar"
 #define  CLASS_NAME  "ebb"
+#define  BASE_ADDR   0x01C20800
+
+#define PA_CFG1_REG *((volatile uint32_t*)(ioremap(BASE_ADDR + 0x04, 4)))
+#define PA_PULL0_REG *((volatile uint32_t*)(ioremap(BASE_ADDR + 0x1C, 4)))
+#define PA_DAT_REG *((volatile uint32_t*)(ioremap(BASE_ADDR + 0x10, 4)))
+
+
+
 MODULE_LICENSE("GPL");
 
 
@@ -28,9 +39,11 @@ static struct file_operations ops = {
 	.release = dev_release,
 };
 
+
 static int __init ebbchar_init(void)
 {
-    printk(KERN_INFO "EBBChar: Initializing the EBBChar LKM\n");
+    
+	printk(KERN_INFO "EBBChar: Initializing the EBBChar LKM\n");
     // Try to dynamically allocated a major number for the device -- more difficult but worth it
 	majorNumber = register_chrdev(0, DEVICE_NAME, &ops);
 	if(majorNumber < 0) 
@@ -58,18 +71,44 @@ static int __init ebbchar_init(void)
 		printk(KERN_ALERT "Failed to create device\n");
 		return PTR_ERR(ebbcharDevice);
 	}
-
+	
 	printk(KERN_INFO "EBBChar: device class created correctly\n");
+	printk(KERN_INFO "EBBChar: sleep 1s\n");
+	
+	
+	printk(KERN_INFO "ivalue of PA_DAT_REG : %X\n", (PA_DAT_REG));
+	printk(KERN_INFO "ivalue of PA_CFG1_REG : %X\n", (PA_CFG1_REG));
+	printk(KERN_INFO "ivalue of PA_CFG1_REG_CHECK : %X\n", ioread32(PA_CFG1_REG_CHECK));
+	printk(KERN_INFO "ivalue of PA_PULL0_REG_CHECK : %X\n", ioread32(PA_PULL0_REG_CHECK));
+	printk(KERN_INFO "ivalue of PA_PULL0_REG : %X\n", (PA_PULL0_REG));
+	
+	PA_CFG1_REG = PA_CFG1_REG & 0xFFFFF111 ; //config output for PA10, PA9, PA8
+	PA_PULL0_REG = PA_PULL0_REG & 0xFFC0FFFF; //remove current value before write new value
+	PA_PULL0_REG = PA_PULL0_REG | (0x2A<<16); //pull down PA10, PA9, PA8
+	msleep(100);
+	PA_DAT_REG = PA_DAT_REG | 0x00000100;
+	ssleep(1);
+	PA_DAT_REG = PA_DAT_REG | 0x00000200;
+	ssleep(1);
+	PA_DAT_REG = PA_DAT_REG | 0x00000400;
+	ssleep(1);
+	printk(KERN_INFO "value of PA_DAT_REG : %X\n", (PA_DAT_REG));
+	printk(KERN_INFO "value of PA_CFG1_REG : %X\n", (PA_CFG1_REG));
+	printk(KERN_INFO "value of PA_CFG1_REG_CHECK : %X\n", ioread32(PA_CFG1_REG_CHECK));
+	printk(KERN_INFO "value of PA_PULL0_REG_CHECK : %X\n", ioread32(PA_PULL0_REG_CHECK));
+	printk(KERN_INFO "value of PA_PULL0_REG : %X\n", (PA_PULL0_REG));
+	printk(KERN_INFO "init driver done\n");
 	return 0;
 }
 
 static void __exit ebbchar_exit(void)
 {
-    device_destroy(ebbcharClass, MKDEV(majorNumber, 0));
+	device_destroy(ebbcharClass, MKDEV(majorNumber, 0));
 	class_unregister(ebbcharClass);
 	class_destroy(ebbcharClass);
 	unregister_chrdev(majorNumber, DEVICE_NAME);
 	printk(KERN_INFO "EBBChar: goodbye from LKM\n");
+	PA_DAT_REG = PA_DAT_REG & 0x00000000;
 }
 
 
