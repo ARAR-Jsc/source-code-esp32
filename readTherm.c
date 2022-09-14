718e78dc9894be2ab6dd80ef6521c3299e7527fd
Truc Do <cda25712@gmail.com>
2022-09-14 18:07:10 +0700

    update read

--- readTherm.c
+++ readTherm.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
+/* this library to use sleep function */
+#include <unistd.h>
 
 char listDir(const char* dirName, char** storeFolderName)
 {
@@ -17,7 +19,6 @@ char listDir(const char* dirName, char** storeFolderName)
 			matchFolder = strstr(entry->d_name, "28-");
 			if(matchFolder != NULL)
 			{
-				printf("dump dir: %s\n", entry->d_name);
 				strcat(storeFolderName[i], "/sys/bus/w1/devices/");
 				strcat(storeFolderName[i], entry->d_name);
 				strcat(storeFolderName[i], "/w1_slave");
@@ -56,6 +57,25 @@ char* readFile(char* fileName)
 }
 
 
+float getTemp(char* fileName)
+{
+	char lineTemp[10];
+	float tempVal;
+	char i;
+	strcpy(lineTemp, strchr(readFile(fileName), '='));
+	// remove '=' in lineTemp
+	for(i = 0; i< sizeof(lineTemp) - 1; i++)
+	{
+		lineTemp[i] = lineTemp[i+1];
+	}
+	lineTemp[sizeof(lineTemp)]='\0';
+	tempVal = (float)atoi(lineTemp)/1000;
+	//printf("dump lineTemp: %2.3f\n",tempVal);
+	return tempVal;
+
+}
+
+
 int main(int argc, char* argv[])
 {
 	char** getNameDS=NULL;
@@ -63,9 +83,9 @@ int main(int argc, char* argv[])
 	char totalFile = 0;
 	char i;
 	int numSlave;
-	char lineTemp[10];
-	float tempVal;
+	
 	getNameDS = (char**)malloc(maxSlave*sizeof(char*));
+	
 	for(i = 0; i < maxSlave; i++)
 	{
 		getNameDS[i] = (char*)malloc(55*sizeof(char));
@@ -73,35 +93,31 @@ int main(int argc, char* argv[])
 	
 	totalFile = listDir("/sys/bus/w1/devices/", getNameDS);
 	
-	for(i = 0; i < totalFile;i++)
-	{
-		printf("\n dumpFIle Name: %s\n", getNameDS[i]);
-	}
-		
-	//for(i = totalFile; i < maxSlave; i++)
-	//{
-	//	free(getNameDS[i]);
-	//}
-	//getNameDS = (char**)realloc(getNameDS,3);
-	
 	numSlave = atoi(readFile("/sys/devices/w1_bus_master1/w1_master_slave_count"));
-	printf("dump num slave: %u\n", numSlave);
-	
+
+	printf("number of slave: %u\n", numSlave);
+
+	if(totalFile != numSlave)
+	{
+		printf("Warning, number of slave and number of result files are different\n");
+	}	
+
+	i = 0; //reset i value
+
 	if(numSlave >= 1)
 	{
 		printf("Found :%u w1 slave\n", numSlave);
-		//realpath("./", buffer);
-		strcpy(lineTemp, strchr(readFile(getNameDS[0]), '='));
-		// remove '=' in lineTemp
-		for(i = 0; i< sizeof(lineTemp) - 1; i++)
+		printf("this is function read temp unstop\n");
+		while(i <= 1)
 		{
-			lineTemp[i] = lineTemp[i+1];
-		}
-		lineTemp[sizeof(lineTemp)]='\0';
-		printf("dump lineTemp: %s\n",lineTemp);
-		tempVal = (float)atoi(lineTemp)/1000;
-		printf("dump lineTemp: %2.3f\n",tempVal);
-		
+			printf("temp at sensor %u: %2.3f\n", i, getTemp(getNameDS[i]));
+			i++;
+			if(i >= totalFile)
+			{
+				sleep(2);
+				i = 0;
+			}
+		}	
 	}
 	else
 	{
